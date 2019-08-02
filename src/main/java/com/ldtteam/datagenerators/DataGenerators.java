38ec8ef21b3d2f10a9e9543ca63c7f9523117bd5
Asherslab <asher.parry@icloud.com>
2019-08-02 17:30:58 +1000

    Add Mod Class

---
+++ src/main/java/com/ldtteam/datagenerators/DataGenerators.java
@@ -0,0 +1,16 @@
+package com.ldtteam.datagenerators;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Have to make this class for weird forge stuff.
+ * otherwise using these classes doesn't work
+ */
+@Mod("datagenerators")
+public class DataGenerators
+{
+    public DataGenerators()
+    {
+        //Empty Public Constructor
+    }
+}
