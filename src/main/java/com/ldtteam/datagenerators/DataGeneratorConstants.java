c2c2f1bba12839fa9cd234d8a773adac618840f7
AshersLab <Asher.parry@icloud.com>
2019-08-02 18:58:25 +1000

    Update DataGeneratorConstants.java

--- src/main/java/com/ldtteam/datagenerators/DataGeneratorConstants.java
+++ src/main/java/com/ldtteam/datagenerators/DataGeneratorConstants.java
@@ -7,8 +7,5 @@ public class DataGeneratorConstants
 {
 
     public static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-
-    // ResourcePack Directories \\
-
-    public static final String EN_US_LANG = "assets/structurize/lang/en_us.json";
+    
 }
